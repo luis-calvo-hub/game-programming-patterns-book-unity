f14562ac3fe3540c66326814cef751b043a60bac
LuisCalvo <itsluisg@gmail.com>
2023-01-09 18:49:56 +0100

    - Fixed a typo issue on the singleton pattern

--- Assets/Patterns/SingletonPattern.cs
+++ Assets/Patterns/SingletonPattern.cs
@@ -1,4 +1,4 @@
-/* Simple example of how a singleton can be used in Unity on a pute C# class, it can also be used inside a MonoBehaviour class
+/* Simple example of how a singleton can be used in Unity on a pure C# class, it can also be used inside a MonoBehaviour class
 * It allows only one instance of a class to be created and gives access to that created instance
  * In most of the times, this pattern should be avoided as it couples the classes to the singletons
  * There are however some use cases and can ease the development of prototypes
